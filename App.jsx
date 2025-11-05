e676f24a9d2d701535c1f82fead7b5337e08982b
vasanthidurai122_97 <61723599+vasanthidurai122-97@users.noreply.github.com>
2025-11-05 15:28:59 +0530

    Refactor App component and update button label

--- App.jsx
+++ App.jsx
@@ -1,39 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
 import React from "react";
 import Header from "./assets/components/Header";
 import Footer from "./assets/components/Footer";
@@ -48,7 +12,7 @@ const App = () =>{
     <>
     <Header title ="My First App" subtitle = "Welcome to website"/>
       <div style={{ textAlign: "center", marginTop: "30px" }}>
-        <Button label="Primary" color="blue" onClick={handleClick} />
+        <Button label="First" color="blue" onClick={handleClick} />
         <Button label="Success" color="green" onClick={() => alert('Success!')} />
         <Button label="Danger" color="red" onClick={() => alert('Danger!')} />
       </div>
